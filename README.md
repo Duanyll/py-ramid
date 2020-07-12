e21d248464484524958d2df3e92701a9c2375afc
duanyll <xiaoguo2004@sina.cn>
2020-07-12 16:23:09 +0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Py-Ramid
+
+A Screeps AI by Duanyll. Don't ask where the name comes from.
+
+Based on [screepers/screeps-typescript-starter](https://github.com/screepers/screeps-typescript-starter).
---
+++ src/types.d.ts
@@ -0,0 +1,17 @@
+// memory extension samples
+interface CreepMemory {
+}
+
+interface Memory {
+}
+
+interface RoomMemory {
+}
+
+// `global` extension samples
+declare namespace NodeJS {
+    interface Global {
+        age: number;
+        log: any;
+    }
+}
